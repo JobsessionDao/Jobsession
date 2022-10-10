ecfee2c79c37eccc17fdd5f20c07f1dc6ed14188
inannan423 <605031236@qq.com>
2022-10-10 13:30:52 +0800

    o

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ let addArticle = require("../../utils/addArticle.js");
 let addLike = require("../../utils/addLike.js");
 let addComment = require("../../utils/addComment.js");
 let addCollect = require("../../utils/addCollect.js");
+// let addLike = require("../../utils/addLike.js");
 // let load = require("../../utils/load.js");
 let count;
 let db;
@@ -20,14 +21,36 @@ Page({
     userAvatar: "",
     userName: "",
     itemList: [],
+
+    navHeight: 0,
+    top: 0,
+    stateHeight: 0,
   },
   onLoad: async function (options) {
+    this.topnav();
     wx.cloud.init();
     db = wx.cloud.database();
     articleList = db.collection("articleList");
     count = await db.collection("articleList").count();
     this.fn();
     this.loadMethod(1);
+  },
+  topnav() {
+    let stateHeight = 0; //  接收状态栏高度
+    let navHeight = wx.getMenuButtonBoundingClientRect().height; //  获取胶囊高度
+    let top = 0;
+    wx.getSystemInfo({
+      success(res) {
+        console.log("状态栏：" + res.statusBarHeight);
+        stateHeight = res.statusBarHeight;
+      },
+    });
+    top = wx.getMenuButtonBoundingClientRect().top - stateHeight; //  获取top值
+    //  然后将取到的值返回在data里面
+    this.setData({
+      navHeight: navHeight + top * 2, //  导航栏高度
+      stateHeight: stateHeight, //  状态栏高度
+    });
   },
   onLaunch: function () {
     // this.getMenuButtonBound();
@@ -42,7 +65,10 @@ Page({
       "校招",
       "这是一篇文章这是一篇文章这是一篇文章这是一篇文章这是一篇文章这是一篇文章这是一篇文章这是一篇文章这是一篇文章这是一篇文章这是一篇文章",
       1,
-      ["https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/20221008104535.png","https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/1665035307457.png"],
+      [
+        "https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/20221008104535.png",
+        "https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/1665035307457.png",
+      ],
       this.data.userName,
       this.data.userID,
       this.data.userAvatar
@@ -75,26 +101,31 @@ Page({
       return "到底了";
     } else {
       return new Promise((resolve, reject) => {
+        // 筛选 articleList 数据表中字段 number 大于 40 的数据,只选出前3条，不进行分页加载
         articleList
           .where({
-            type: aType,
+            number: db.command.gt(1),
           })
-          .skip(this.data.itemList.length)
-          .get()
-          .then((res) => {
-            console.log(res.data);
-            console.log(old_data.concat(res.data).length);
-            if (old_data.concat(res.data).length === allLength) {
-              isAll = true;
-            }
-            allLength = old_data.concat(res.data).length;
-            this.setData({
-              itemList: old_data.concat(res.data),
-            });
-            // resolve(old_data.concat(res.data.data));
+          .limit(3)
+          .orderBy("number", "desc")
+          .get({
+            success: (res) => {
+              this.setData({
+                itemList: res,
+              });
+              console.log(res);
+              resolve("加载成功");
+            },
           });
       });
     }
+  },
+  addLike: async function (e) {
+    let res = await addLike.addLike.addMethod(
+      "6cf53e9d63413019006937fb24b67c3d",
+      this.data.userID
+    );
+    console.log(res);
   },
   async login() {
     // 用户登录
